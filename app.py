763d4b3add0e8c928457ef4b3e5e1978d979f477
Wilson McDade <wilson.mcdade@gmail.com>
2024-03-28 00:01:46 -0400

    Some work on member card

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivymd.uix.boxlayout import BoxLayout
+from kivymd.uix.card import MDCard
 
+from kivy.properties import StringProperty
 from kivy.lang import Builder
 from kivy.config import Config
+from kivy.core.window import Window
+
+class MemberCard(MDCard):
+    name = StringProperty()
+    job_title = StringProperty()
+    major = StringProperty()
 
 class MainApp(MDApp):
     def build(self):
-
-        Config.set('graphics', 'resizable', False)
-        Config.set('graphics', 'width', '1024')
-        Config.set('graphics', 'height', '600')
+        Window.size = (1024,600)
         
         self.root = BoxLayout()
 
         self.root.add_widget(Builder.load_file('app.kv'))
+
+        print(self.root.ids)
+
+        self.root.add_widget(
+            MemberCard(name="Wilson", job_title="Cool guy", major="CS")
+        )
 
 if __name__ == "__main__":
 
