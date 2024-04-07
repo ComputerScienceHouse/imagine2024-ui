e795a711cc8dfd59a37895af0303d1a534a10eba
IsaacIngram <me@isaacingram.dev>
2024-04-06 23:05:38 -0400

    implement eq and str functions for Item and User

--- models.py
+++ models.py
@@ -1,3 +1,7 @@
+
+WEIGHT_UNIT = "g"
+
+
 class User:
     def __init__(self, uid, name, token, balance, payment_type, email, phone):
         self.uid = uid
@@ -7,6 +11,22 @@ class User:
         self.payment_type = payment_type
         self.email = email
         self.phone = phone
+
+    def __eq__(self, other):
+        if isinstance(other, User):
+            return (self.uid == other.uid
+                    and self.name == other.name
+                    and self.token == other.token
+                    and self.balance == other.balance
+                    and self.payment_type == other.payment_type
+                    and self.email == other.email
+                    and self.phone == other.phone)
+        else:
+            return False
+
+    def __str__(self):
+        return (f'User[{self.uid},{self.name},{self.token},${self.balance},'
+                f'{self.payment_type},{self.email},{self.phone}]')
 
 
 class Item:
@@ -23,6 +43,26 @@ class Item:
         self.std_weight = std_weight
         self.thumbnail = thumbnail
         self.vision_class = vision_class
+
+    def __eq__(self, other):
+        if isinstance(other, Item):
+            return (self.item_id == other.item_id
+                    and self.name == other.name
+                    and self.upc == other.upc
+                    and self.price == other.price
+                    and self.units == other.units
+                    and self.avg_weight == other.avg_weight
+                    and self.std_weight == other.std_weight
+                    and self.thumbnail == other.thumbnail
+                    and self.vision_class == other.vision_class)
+        else:
+            return False
+
+    def __str__(self):
+        return (f'Item[{self.item_id},{self.name},UPC:{self.upc},${self.price},'
+                f'{self.units}units,{self.avg_weight}{WEIGHT_UNIT},'
+                f'{self.std_weight}{WEIGHT_UNIT},{self.thumbnail},'
+                f'{self.vision_class}]')
 
 
 class Stock:
