ec47e28ebc469f57299e508fc333f81a0bfd060b
IsaacIngram <me@isaacingram.dev>
2024-04-10 00:11:36 -0400

    rewrite rfid to run as an independent program

--- utils/rfid_reader.py
+++ utils/rfid_reader.py
@@ -8,8 +8,6 @@
 #
 ###############################################################################
 import platform
-import threading
-from typing import Any
 
 USE_RFID = False
 
@@ -20,70 +18,20 @@ if platform.system() == "Linux" and "rpi" in platform.uname().release:
     from mfrc522 import SimpleMFRC522
 
 
-class RFIDReader:
+def main():
 
-    # reader: SimpleMFRC522 | None
-    read_thread: threading.Thread | None
-    _done_reading_flag: bool
-    last_card_value: Any
+    # Check if RFID can be used
+    if USE_RFID:
 
-    def __init__(self):
-        """
-        Initialize this RFID reader
-        """
-        self.read_thread = None
-        self.last_card_value = None
-        self._done_reading_flag = False
-        if USE_RFID:
-            self.reader = SimpleMFRC522()
-        else:
-            self.reader = None
+        reader = SimpleMFRC522()
+        try:
+            while True:
+                tag_uid, _ = reader.read()
+        except KeyboardInterrupt:
+            print("Control+C pressed, exiting program")
+        finally:
+            GPIO.cleanup()
 
-    def _read_card(self):
-        """
-        Function responsible for reading the RFID card. This should be the
-        target function of the thread.
-        :return:
-        """
-        self.last_card_value, _ = self.reader.read()
-        self._done_reading_flag = True
 
-    def start_read(self) -> None:
-        """
-        Start looking for cards. This just signals the thread to
-        begin reading cards.
-        :return: None
-        """
-        if USE_RFID:
-            # Spawn a read thread
-            self.read_thread = threading.Thread(target=self._read_card)
-            self.read_thread.start()
-
-    def waiting_for_card(self) -> bool:
-        """
-        Check if the reader is waiting for a card. Will return True if the
-        reader is running and waiting for a card. Once a card is read or the
-        stop_read() function is called, this function will return False.
-        :return: True if the reader is waiting on a card, False otherwise
-        """
-        if USE_RFID:
-            if not self._done_reading_flag:
-                # Not done reading, return True
-                return True
-            else:
-                # Finished reading, return False
-                self._done_reading_flag = False
-                return False
-        else:
-            return False
-            pass
-
-    def get_last_read_value(self):
-        """
-        Get the value of the card that was last read
-        :return:
-        """
-        return self.last_card_value
-
-    def cleanup(self):
-        GPIO.cleanup()
+if __name__ == '__main__':
+    main()
