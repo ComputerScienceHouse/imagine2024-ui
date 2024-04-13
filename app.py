627dce0fa2daf2d0255ba152651b0e016a60387c
IsaacIngram <me@isaacingram.dev>
2024-04-13 18:29:59 -0400

    make cart scrollable

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from kivy.core.image import Image
 from kivy.loader import Loader
+from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivymd.uix.boxlayout import BoxLayout
@@ -8,7 +9,7 @@ from kivymd.uix.screenmanager import ScreenManager
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.list.list import MDListItem
 
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ListProperty
 from kivy.lang import Builder
 from kivy.config import Config
 from kivy.core.window import Window
@@ -20,12 +21,18 @@ class InfoScreen(MDScreen):
     pass
 
 
-class CartScreen(MDScreen):
-    pass
+class CartScreen(Screen):
+    cart_items = ListProperty([])
+
+    def refresh_cart(self):
+        cart_view = self.children[1].children[0]
+        cart_view.data = [{'title': item['title'], 'source': item['source'], 'price': item['price'], 'quantity': item['quantity']} for item in self.cart_items]
+        cart_view.refresh_from_data()
 
 
 class StartScreen(MDScreen):
     pass
+
 
 class AttractScreen(MDScreen):
     pass
@@ -60,8 +67,19 @@ class MainApp(MDApp):
 
         self.root.add_widget(Builder.load_file('app.kv'))
 
-        self.root.children[0].current = 'Start'
-        self.root.children[0].current = 'Start'
+        cart_screen = self.root.children[0].get_screen('Cart')
+        cart_screen.cart_items = [
+            {'title': 'Jolt Soda', 'source': 'images/oof.png', 'price': '3.00', 'quantity': '3'},
+            {'title': 'Coffee', 'source': 'images/oof.png', 'price': '2.50', 'quantity': '2'},
+            {'title': 'Tea', 'source': 'images/oof.png', 'price': '1.75', 'quantity': '5'},
+            {'title': 'Orange Juice', 'source': 'images/oof.png', 'price': '4.50', 'quantity': '7'},
+            {'title': 'Water', 'source': 'images/oof.png', 'price': '1.00', 'quantity': '4'},
+            {'title': 'Smoothie', 'source': 'images/oof.png', 'price': '5.25', 'quantity': '6'},
+            {'title': 'Milkshake', 'source': 'images/oof.png', 'price': '3.75', 'quantity': '9'},
+            {'title': 'Lemonade', 'source': 'images/oof.png', 'price': '2.25', 'quantity': '8'},
+            {'title': 'Iced Tea', 'source': 'images/oof.png', 'price': '2.00', 'quantity': '1'}
+        ]
+        cart_screen.refresh_cart()
 
 
 if __name__ == "__main__":
