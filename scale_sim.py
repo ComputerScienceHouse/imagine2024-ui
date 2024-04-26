bd03a7f71a2098f50421968a040186623c5eee3a
IsaacIngram <me@isaacingram.dev>
2024-04-26 03:38:45 -0400

    initial commit of scale sim

---
+++ scale_sim.py
@@ -0,0 +1,48 @@
+import models
+import time
+
+
+def simulate_value(slot, value):
+    print(f"Weight simulated as {value}")
+    slot.update(value)
+    time.sleep(1)
+
+
+def main():
+    shelf = models.Shelf()
+    item = models.Item(1, 'test', '', 3, 20, 226, 10, '', '')
+    slot = models.Slot(shelf, item)
+    # slot.set_conversion_factor(0.22668)
+
+    simulate_value(slot, 4)
+    simulate_value(slot, 4)
+    simulate_value(slot, 4)
+    simulate_value(slot, 230)
+    simulate_value(slot, 230)
+    simulate_value(slot, 230)
+    simulate_value(slot, 2)
+    simulate_value(slot, 2)
+    simulate_value(slot, 2)
+    simulate_value(slot, 2)
+    simulate_value(slot, 2)
+    simulate_value(slot, 2)
+    simulate_value(slot, 2)
+    simulate_value(slot, 4)
+    simulate_value(slot, 4)
+    simulate_value(slot, 4)
+    simulate_value(slot, 2)
+    simulate_value(slot, 2)
+    simulate_value(slot, 2)
+    simulate_value(slot, 2)
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
