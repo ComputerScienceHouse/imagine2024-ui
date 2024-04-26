6ce7236feeda1bc244248ad446c20c7c79e2a0c9
IsaacIngram <me@isaacingram.dev>
2024-04-26 03:43:01 -0400

    add callback for updating data from scales

--- utils/mqtt.py
+++ utils/mqtt.py
@@ -12,17 +12,19 @@ from typing import Callable
 from typing import Any
 import database
 
-MQTT_BROKER_ADDRESS = "localhost"
+MQTT_BROKER_ADDRESS = "test.mosquitto.org"
 MQTT_BROKER_PORT = 1883
 
 
 RFID_USER_TOPIC = "rfid/user"
+SHELF_DATA_TOPIC = "shelf/data"
 
 
 class MQTT:
 
     client: mqtt.Client
     _rfid_user_callback: Callable[[str], Any] | None
+    _shelf_data_callback: Callable[[str], Any] | None
 
     def __init__(self):
         # Create client
@@ -31,12 +33,15 @@ class MQTT:
 
         # Add callback functions
         self.client.message_callback_add(RFID_USER_TOPIC, self._internal_rfid_user_callback)
+        self.client.message_callback_add(SHELF_DATA_TOPIC, self._internal_shelf_data_callback)
 
         # Subscribe to topics
         self.client.subscribe(RFID_USER_TOPIC)
+        self.client.subscribe(SHELF_DATA_TOPIC)
 
         # Set all callback functions to None
         self._rfid_user_callback = None
+        self._shelf_data_callback = None
 
     def _internal_rfid_user_callback(self, client: mqtt.Client, userdata: Any, message: mqtt.MQTTMessage):
         """
@@ -51,6 +56,17 @@ class MQTT:
         if self._rfid_user_callback is not None:
             self._rfid_user_callback(message.payload.decode("utf-8"))
 
+    def _internal_shelf_data_callback(self, client: mqtt.Client, userdata: Any, message: mqtt.MQTTMessage):
+        """
+        Internal callback for the
+        :param client:
+        :param userdata:
+        :param message:
+        :return:
+        """
+        if self._shelf_data_callback is not None:
+            self._shelf_data_callback(message.payload.decode("utf-8"))
+
     def set_rfid_user_callback(self, callback: Callable[[str], Any]):
         """
         Set callback function for when a user is read with RFID
@@ -58,6 +74,14 @@ class MQTT:
         :return:
         """
         self._rfid_user_callback = callback
+
+    def set_shelf_data_callback(self, callback: Callable[[str], Any]):
+        """
+
+        :param callback:
+        :return:
+        """
+        self._shelf_data_callback = callback
 
     def start_listening(self):
         self.client.loop_start()
